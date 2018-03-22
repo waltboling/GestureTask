c7e63397edbf00302000789a8b52398a9768a5bb
Walt Boling <bolingjw@gmail.com>
2018-03-22 15:14:40 -0400

    Initial Commit

---
+++ GestureTask/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GestureTask
+//
+//  Created by Jon Boling on 3/22/18.
+//  Copyright © 2018 Walt Boling. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
